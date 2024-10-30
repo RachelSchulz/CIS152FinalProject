adf29a7b46db0fa6c45fea1f0b74d5b0987cc30d
RachelSchulz <rachelsbeckwith@gmail.com>
2024-10-30 11:22:44 -0500

    AddedRecipeList
    Added RecipeList class and driver

---
+++ src/recipe_app/RecipeAppDriver.java
@@ -0,0 +1,33 @@
+/** Recipe App stores user's favorite recipes
+ * @author Rachel Schulz
+ * @date Oct 30, 2024
+ * @version 1.0
+ * @since 1.0
+*/
+/*  
+* OS: macOS 14.6.1
+* IDE: Eclipse
+* Copyright : This is my own original work 
+* based on specifications issued by our instructor
+* Academic Honesty: I attest that this is my original work.
+* I have not used unauthorized source code, either modified or
+* unmodified, nor used generative AI as a final draft. 
+* I have not given other fellow student(s) access to my program.
+*/
+
+package recipe_app;
+
+/**
+ * 
+ */
+public class RecipeAppDriver {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
---
+++ src/recipe_app/RecipeList.java
@@ -0,0 +1,34 @@
+/**
+ * @author Rachel Schulz
+ * @date Oct 30, 2024
+ */
+package recipe_app;
+
+import java.util.ArrayList;
+
+public class RecipeList {
+	
+	private ArrayList<Recipe> recipes;
+	
+	//AddRecipe
+	public void addRecipe() {
+		//TODO
+	}
+	
+	public void deleteRecipe() {
+		//TODO
+	}
+	
+	public void editRecipe() {
+		//TODO
+	}
+	
+	public void displayAllRecipes() {
+		//TODO
+	}
+	
+	public void searchForRecipe() {	//TODO RETURN RECIPE AS TYPE
+		//TODO
+	}
+	
+}
