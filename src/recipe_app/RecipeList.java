75e96f3941097ad73a45156f1dc7dd022c8898e8
RachelSchulz <rachelsbeckwith@gmail.com>
2024-11-13 12:35:42 -0600

    RecipeListMethods
    Added RecipeList methods and some tests

--- src/recipe_app/RecipeList.java
+++ src/recipe_app/RecipeList.java
@@ -10,25 +10,98 @@ public class RecipeList {
 	
 	private ArrayList<Recipe> recipes;
 	
-	//AddRecipe
-	public void addRecipe() {
-		//TODO
+	//constructor
+	public RecipeList() {
+		this.recipes = new ArrayList<Recipe>();
 	}
 	
-	public void deleteRecipe() {
-		//TODO
+	//AddRecipe
+	public void addRecipe(Recipe r) {
+		recipes.add(r);
+	}
+	
+	public void deleteRecipe(Recipe r) {
+		recipes.remove(r);
+	}
+	
+	public void deleteRecipe(int i) {
+		recipes.remove(i);
 	}
 	
 	public void editRecipe() {
 		//TODO
 	}
 	
-	public void displayAllRecipes() {
-		//TODO
+	/**
+	 * 
+	 * @return ArrayList recipes sorted in ascending order by title
+	 */
+	public String displayAllRecipes() {
+		String recipeString = new String();
+		//recipes.sortRecipes();	//WHY IS METHOD UNDEFINED FOR TYPE ARRAYLIST<RECIPE> FIX ME!
+		for (Recipe r : recipes) {
+			recipeString += r.getTitle() + "\n|";
+		}
+		return recipeString;
 	}
 	
-	public void searchForRecipe() {	//TODO RETURN RECIPE AS TYPE
-		//TODO
+	/**
+	 * @return ArrayList<Recipe> with all recipes that contain keyword in title
+	 * returns empty ArrayList if not found
+	 */
+	public ArrayList<Recipe> searchForRecipe(String keyword) {
+		ArrayList<Recipe> results = new ArrayList<Recipe>();
+		for (int i = 0; i < recipes.size(); i++) {
+			if (recipes.get(i).getTitle().toLowerCase().contains(keyword.toLowerCase())) {
+				results.add(recipes.get(i));
+			}
+		}
+		return results;
+	}
+	
+	public boolean recipeIsInList(String keyword) {
+		for (int i = 0; i < recipes.size(); i++) {
+			if (recipes.get(i).getTitle().toLowerCase().contains(keyword.toLowerCase())) {	//returns true if keyword found in any title
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * @return ArrayList<Recipe> with all recipes that contain ingredient in IngredientList
+	 * returns empty ArrayList if not found
+	 */
+	public ArrayList<Recipe> searchRecipeByIngredient(String ing) {
+		ArrayList<Recipe> results = new ArrayList<Recipe>();
+		for (int i = 0; i < recipes.size(); i++) {
+			if (recipes.get(i).displayIngredients().toLowerCase().contains(ing.toLowerCase())) {	//if ingredients list of recipe contains ingredient
+				results.add(recipes.get(i));
+			}
+		}
+		return results;
+	}
+	
+	/**
+	 * @return size of RecipeList
+	 */
+	public int getSize() {
+		return recipes.size();
+	}
+	
+	/**
+	 * @return index of recipe if found
+	 * returns -1 if not found
+	 */
+	public int getIndexOfRecipe(Recipe r) {
+		return recipes.indexOf(r);
+	}
+	
+	/**
+	 * sorts recipes in list in ascending order by title
+	 */
+	public void sortRecipes() {
+		recipes.sort(new RecipeComparator());
 	}
 	
 }
--- src/tests/RecipeListTests.java
+++ src/tests/RecipeListTests.java
@@ -3,10 +3,73 @@
  * @date Nov 4, 2024
  */
 package tests;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+
+import recipe_app.Ingredient;
+import recipe_app.IngredientList;
+import recipe_app.Recipe;
+import recipe_app.RecipeList;
+
 
 /**
  * 
  */
 public class RecipeListTests {
-
+	@Test
+	public void addRecipe() {
+		//ARRANGE
+		Recipe r1 = new Recipe("Tofu Scramble");
+		RecipeList rlist = new RecipeList();
+		//ACT
+		rlist.addRecipe(r1);
+		int rlsize = rlist.getSize();
+		int expectedSize = 1;
+		//ASSERT
+		assertEquals(rlsize, expectedSize);
+	}
+	
+	@Test
+	public void sortRecipes() {
+		//ARRANGE
+		Recipe r1 = new Recipe("Tofu Scramble");
+		Recipe r2 = new Recipe("Apple Pie");
+		Recipe r3 = new Recipe("Banana Pudding");
+		RecipeList rlist = new RecipeList();
+		rlist.addRecipe(r1);
+		rlist.addRecipe(r2);
+		rlist.addRecipe(r3);
+		int expectedIndex = 0;	//expected index of r2 Apple Pie after sortRecipes call
+		//ACT
+		rlist.sortRecipes();
+		//ASSERT
+		assertEquals(rlist.getIndexOfRecipe(r2), expectedIndex);
+	}
+	
+	@Test
+	public void ingredientFoundInSearch() {
+		//ARRANGE
+		Ingredient carrots = new Ingredient("carrots", 1.5, "cups", "diced");
+		Ingredient onions = new Ingredient("onions", 1, "cup", "chopped");
+		Ingredient celery = new Ingredient("celery");
+		Ingredient lettuce = new Ingredient("lettuce", 3, "cups");
+		Ingredient ranch = new Ingredient("Ranch dressing", 1.5, "cups");
+		IngredientList ingredients1 = new IngredientList();
+		ingredients1.addIngredient(carrots);
+		ingredients1.addIngredient(onions);
+		ingredients1.addIngredient(celery);
+		ingredients1.addIngredient(lettuce);
+		ingredients1.addIngredient(ranch);
+		Recipe salad = new Recipe("Salad", ingredients1, "directions");
+		RecipeList rlist = new RecipeList();
+		rlist.addRecipe(salad);
+		//ACT
+		
+		//ASSERT
+		assertEquals(rlist, rlist.searchRecipeByIngredient("carrot"));
+		//not sure how to make this test make sense
+	}
 }
